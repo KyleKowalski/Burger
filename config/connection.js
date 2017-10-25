8d4414f8593d09102e49880c3f59d1454353f8d9
Kyle Kowalski <Kyle.Kowalski@gmail.com>
2017-10-24 18:14:47 -0600

    minor tweak to previous connection

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,11 @@
 const mysql = require('mysql');
+let connection;
+
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  var connection = mysql.createConnection({
+    connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : 'Luca1234',
